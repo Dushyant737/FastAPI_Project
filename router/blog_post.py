bfbc8cb370b6f99279098c8b9978f9504d7fdb3b
Dushyant737 <dushyant737@gmail.com>
2023-01-06 21:08:56 +0530

    added new Postcode

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter 
+from fastapi import APIRouter , Query , Body , Path
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional , List , Dict
 
 router = APIRouter(
     prefix='/blog',
@@ -42,6 +42,7 @@ def create_blog(blog: BlogModel):
 
 # Path and query parameters in Post 
 
+"""
 class BlogModel(BaseModel):
     title : str
     name : str
@@ -49,6 +50,178 @@ class BlogModel(BaseModel):
     nb_comments : int
     published : Optional[bool]
     
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel , id : int , version : int =1 ):
+    return {
+        'id' : id,
+        'Data':blog,
+        'version' : version
+        }
+"""
+
+# Program for metadata (Title, Descption, Alias)
+    
+"""   
+
+class BlogModel(BaseModel):
+    title : str
+    name : str
+    content : str
+    nb_comments : int
+    published : Optional[bool]
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel , id : int,
+        comment_id: int = Query(None,
+            title="Id of the comment",
+            description="some description of the comment",
+           # alias="commentId",
+           # deprecated=True
+        )):
+    return {
+        'blog' : blog,
+        'id' : id,
+        'Data':blog,
+        'comment_id' : comment_id
+        }
+"""
+
+# added body peremeter (optional, fixed, validator - max/min/regex)
+
+"""
+
+class BlogModel(BaseModel):
+    title : str
+    name : str
+    content : str
+    nb_comments : int
+    published : Optional[bool]
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel , id : int,
+        comment_id: int = Query(None,
+            title="Id of the comment",
+            description="some description of the comment",
+            alias="commentId",
+            deprecated=True
+        ),
+        # content: str = Body("Hi, How are you ?") # it's optinal 
+        # content: str = Body(Ellipsis)
+        content: str = Body(...,
+        min_length=10,
+        max_length=20,
+        regex='[a-z\s]*$'
+        )
+        
+    ):   
+    return {
+        'blog' : blog,
+        'id' : id,
+        'Data':blog,
+        'comment_id' : comment_id
+        }
+"""        
+
+# multiple values with list in query peremeters 
+
+"""
+
+class BlogModel(BaseModel):
+    title : str
+    name : str
+    content : str
+    nb_comments : int
+    published : Optional[bool]
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel , id : int,
+        comment_id: int = Query(None,
+            title="Id of the comment",
+            description="some description of the comment",
+            alias="commentId",
+            deprecated=True
+        ),
+        # content: str = Body("Hi, How are you ?") # it's optinal 
+        # content: str = Body(Ellipsis)
+        content: str = Body(...,
+        min_length=10,
+        max_length=20,
+        regex='[a-z\s]*$'
+        ),
+        # v: Optional[List[str]] = Query(None)
+        v: Optional[List[str]] = Query(['1.0','1.1','1.2']),
+        
+    ): 
+
+    return {
+        'blog' : blog,
+        'id' : id,
+        'Data':blog,
+        'comment_id' : comment_id,
+        'Vesrion' : v,
+        }
+"""        
+
+# Number Peremeres
+
+"""
+class BlogModel(BaseModel):
+    title : str
+    name : str
+    content : str
+    nb_comments : int
+    published : Optional[bool]
+
+
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel , id : int,
+        comment_title: int = Query(None,
+            title="title of the comment",
+            description="some description of the comment_title",
+            alias="commentTitle",
+            deprecated=True
+        ),
+        # content: str = Body("Hi, How are you ?") # it's optinal 
+        # content: str = Body(Ellipsis)
+        content: str = Body(...,
+        min_length=10,
+        max_length=20,
+        regex='[a-z\s]*$'
+        ),
+        # v: Optional[List[str]] = Query(None)
+        v: Optional[List[str]] = Query(['1.0','1.1','1.2']),
+        comment_id: int = Path(None,gt=5, le=10)
+    ): 
+
+    return {
+        'blog' : blog,
+        'id' : id,
+        'Data':blog,
+        'comment_title' : comment_title,
+        'Vesrion' : v,
+        'comment_id' : comment_id
+        }
+"""
+
+# complex subtypes
+
+class Image(BaseModel):
+    url: str
+    alias: str
+
+class BlogModel(BaseModel):
+    title : str
+    name : str
+    content : str
+    nb_comments : int
+    published : Optional[bool]
+    tags : List[str] = []
+    metadata : Dict[str,str] = {'key1' : 'value1'}
+    image:Optional[Image] = None
 
 @router.post('/new/{id}')
 def create_blog(blog: BlogModel , id : int , version : int =1 ):
